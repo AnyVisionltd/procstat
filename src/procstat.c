AnyVisionltd/procstat

commit bcba0134f222c35f8e7344617d99a88d1cfee093

fix memory corruption by read from removed items
An item unregistered via procstat_remove may still be reached by fuse_read():
the item itself has refcnt held from fuse_open.
In such case, the owner may have freed the item stat memory,
which is still ok to read.

HOWEVER, writing to this memory is prohibited and may cause memory corruption.
Write is possible only for series (see is_reset() and clear_values_...)
called from e.g. series_u64_read().
The item itself may not be marked as unregistered (refcnt != 0 in item_put_locked),
but since series are removed by directory we can rely on parent
being marked as unregistered by procstat_remove().

Same applies to reads by aggregator_read().

NOTE: after procstat_remove() releases the mutex, changes to the item (flags)
should be observable by fuse_read() and aggregator_read() from other cpus.
- fuse_read() does not take the mutex, so this is suspicious.
- aggregator_read() does, so the protection is clean.

The recommendation is not not read series stats that may be removed
(volumes) unless via an aggregator.

Issue: LBM1-15406

Signed-off-by: Anton Eidelman <anton@lightbitslabs.com>

src/procstat.c
@@ -546,6 +546,10 @@ static int out_item(struct out_stream *out, char *path, struct procstat_item *it
 		int p_space = MAX_PATH_LEN - path_len;
 		struct procstat_directory *dir = container_of(item, struct procstat_directory, base);
 
+		/* See fuse_read(): it is unsafe to read files under a directory that is marked unregistered */
+		if (!item_registered(item))
+			return 0;
+
 		if (pos && p_space) {
 			path[pos++] = '/';
 			--p_space;
@@ -693,7 +697,15 @@ static void fuse_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, st
 		return;
 	}
 
-	if (!file->fmt) {
+	/*
+	 * An item unregistered via procstat_remove may still be reached here: the item itself has refcnt held from fuse_open.
+	 * If so, the owner may have freed the item stat memory, which is still ok to read.
+	 * HOWEVER, writing to this memory is prohibited and may cause memory corruption.
+	 * Write is possible only for series (see is_reset() and clear_values_...).
+	 * The item itself may not be marked as unregistered (refcnt != 0 in item_put_locked),
+	 * but since series are removed by directory we can rely on parent being marked as unregistered by procstat_remove().
+	 */
+	if (!file->fmt || !file->base.parent || !item_registered(&file->base.parent->base)) {
 		fuse_reply_buf(req, NULL, 0);
 		return;
 	}

commit 84fc070d5ce3ac5a6e0f12b796d6c704beebc3b4

core: better fix for bcba0134f222c35f8e7344617d99a88d1cfee093
In previous fix we made some artifical checks in order not to read
stale memory of an item that was removed after "open" was called.

Instead of this lets improve the remove mechanism and do "unregister" of
all items "below" the current item in the tree. (than user can safely
remove the memory backing those variables). Now since the
"procstat_item" is still alive (held by open operation), we will check
the "registration" status of the variable and disallow the "read" in
this case since item is already unregistered.

Fixes: bcba013 ("fix memory corruption by read from removed items")

src/procstat.c
@@ -700,16 +700,14 @@ static void fuse_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, st
 		return;
 	}
 
-	/*
-	 * An item unregistered via procstat_remove may still be reached here: the item itself has refcnt held from fuse_open.
-	 * If so, the owner may have freed the item stat memory, which is still ok to read.
-	 * HOWEVER, writing to this memory is prohibited and may cause memory corruption.
-	 * Write is possible only for series (see is_reset() and clear_values_...).
-	 * The item itself may not be marked as unregistered (refcnt != 0 in item_put_locked),
-	 * but since series are removed by directory we can rely on parent being marked as unregistered by procstat_remove().
-	 */
-	if (!file->fmt || !file->base.parent || !item_registered(&file->base.parent->base)) {
-		fuse_reply_buf(req, NULL, 0);
+
+	if (!item_registered(&file->base)) {
+		fuse_reply_err(req, ENOENT);
+		return;
+	}
+
+	if (!file->fmt) {
+		fuse_reply_err(req, EPERM);
 		return;
 	}
 
@@ -822,13 +820,12 @@ static void item_put_locked(struct procstat_item *item)
 {
 	assert(item->refcnt);
 
-	if (--item->refcnt)
-		return;
-
 	item->flags &= ~STATS_ENTRY_FLAG_REGISTERED;
 	if (item_type_directory(item))
 		item_put_children_locked((struct procstat_directory *)item);
 
+	if (--item->refcnt)
+		return;
 	free_item(item);
 }
 

test/test_c.cpp
@@ -80,6 +80,22 @@ TEST_F (ProcstatTest, test_create_dirs) {
 }
 
 
+TEST_F (ProcstatTest, test_remove_item_not_visible_from_parent) {
+	struct procstat_item *item, *item2, *item3;
+
+	item = procstat_create_directory(context, nullptr, "outer");
+	ASSERT_TRUE(boost::filesystem::exists(mount_name() + "/outer"));
+	item2 = procstat_create_directory(context, item, "inner");
+	ASSERT_TRUE(boost::filesystem::exists(mount_name() + "/outer/inner"));
+	item3 = procstat_lookup_item(context, item, "inner");
+	ASSERT_EQ(item2, item3);
+
+	procstat_remove(context, item3);
+	ASSERT_FALSE(boost::filesystem::exists(mount_name() + "/outer/inner"));
+	item3 = procstat_lookup_item(context, item, "inner");
+	ASSERT_TRUE(!item3);
+}
+
 TEST_F (ProcstatTest, test_create_invalid_filename) {
 	u64 dummy;
 	int error;
@@ -457,4 +473,52 @@ TEST_F (ProcstatTest, test_control)
 	procstat_remove(context, item);
 }
 
+TEST_F (ProcstatTest, test_delete_stat_after_open)
+{
+	ifstream read_try;
+
+	int *value;
+	int error;
+
+	value = (int *)malloc(sizeof(int));
+	*value = 10;
+
+	error = procstat_create_int_parameter(context, NULL, "param", value);
+	ASSERT_TRUE(!error);
+
+	ASSERT_EQ(10, read_stat_file<int>(mount_name() + "/param"));
+
+	// Open the file
+	read_try.open(mount_name() + "/param");
+	procstat_remove_by_name(context, NULL, "param");
+	free(value);
+
+	ASSERT_EQ(-1, read_try.peek());
+}
+
+
+TEST_F (ProcstatTest, test_delete_via_root_dir_after_open)
+{
+	ifstream read_try;
+
+	int *value;
+	int error;
+
+	value = (int *)malloc(sizeof(int));
+	*value = 10;
+
+	struct procstat_item *item1 = procstat_create_directory(context, NULL, "dir1");
+	struct procstat_item *item2 = procstat_create_directory(context, item1, "dir2");
+
+	error = procstat_create_int_parameter(context, item2, "param", value);
+	ASSERT_TRUE(!error);
+	ASSERT_EQ(10, read_stat_file<int>(mount_name() + "/dir1/dir2/param"));
+
+	// Open the file
+	read_try.open(mount_name() + "/dir1/dir2/param");
+	procstat_remove_by_name(context, NULL, "dir1");
+	free(value);
+
+	ASSERT_EQ(-1, read_try.peek());
+}
 